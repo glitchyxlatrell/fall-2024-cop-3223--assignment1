76ef1091879e87c33760331458ed34c53fa13168
glitchyxlatrell <la586421@ucf.edu>
2024-09-06 07:00:53 +0000

    adding comments and fixing styling

--- fracturing.c
+++ fracturing.c
@@ -28,8 +28,10 @@ double askForUserInput(){
 }
 
 double findDistance(){
-    //function that gets the distance between two points, but only outputs the position of the points and returns the distance value
-     double x1 = askForUserInput();  //asking user input for x1
+    //function that gets the distance between two points, but only outputs the position of the points 
+    //and returns the distance value
+
+    double x1 = askForUserInput();  //asking user input for x1
     double x2 = askForUserInput();  //asking user input for x2
     double y1 = askForUserInput();  //asking user input for y1
     double y2 = askForUserInput();  //asking user input for y2
@@ -37,7 +39,6 @@ double findDistance(){
     printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
     printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     //print statements that tell the position of the points entered
-
 
     double distance = sqrt(pow(x2-x1,2)+ pow(y2-y1,2));
     //calculating distance between the points
@@ -70,8 +71,12 @@ double calculateDistance(){
 double calculatePerimeter(){
     //function that will calculate perimeter of a circle encompassing a city and outputs the value
 
-    double radius = findDistance()/2;   //using calculateDistance function to get diameter then dividing by half to find radius
-    double perimeter = 2*PI*radius;     //finding perimeter
+    double radius = findDistance()/2;   
+    //using calculateDistance function to get diameter then dividing by half to find radius
+
+    double perimeter = 2*PI*radius;     
+    //finding perimeter using circumference of a circle formula
+
     printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
     //outputting the perimeter of circle
 
@@ -83,8 +88,11 @@ double calculatePerimeter(){
 double calculateArea(){
     //function that will calculate the area of a circle given two points which indicate it's diameter
 
-    double radius = findDistance()/2;   //using calculateDistance function to get diameter then dividing by half to find radius
-    double area = PI*pow(radius,2);     //calculating the area of the cirlce given two points
+    double radius = findDistance()/2;
+    //using calculateDistance function to get diameter then dividing by half to find radius
+
+    double area = PI*pow(radius,2);
+    //calculating the area of the cirlce using area of a circle formula
 
     printf("The area of the city encompassed by your request is %lf\n", area);
     //outputting area of the circle
@@ -94,7 +102,8 @@ double calculateArea(){
 }
 
 double calculateWidth(){
-    double width = findDistance();      //since width is equal to diameter, width is set equal to the return of findDistance
+    double width = findDistance();
+    //since width is equal to diameter, width is set equal to the return of findDistance
 
     printf("The width of the city encompassed by your request is %lf\n", width);
     //outputting width of the circle
@@ -104,7 +113,8 @@ double calculateWidth(){
 }
 
 double calculateHeight(){
-    double height = findDistance();      //since height is equal to diameter, width is set equal to the return of findDistance
+    double height = findDistance();
+    //since height is equal to diameter, width is set equal to the return of findDistance
 
     printf("The height of the city encompassed by your request is %lf\n", height);
     //outputting height of the circle
@@ -112,6 +122,9 @@ double calculateHeight(){
      return 1.0;
     //returing a double that indicates my difficulty level with this function  
 }
+
+
+//main code that calls functions to calculate aspects of a circle given two points
 
 int main(int argc, char **argv){
     calculateDistance();
